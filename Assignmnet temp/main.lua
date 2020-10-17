120aa246954a15610334c5da8230ad4837e3ab69
Lecrea <58100384+Lecrea@users.noreply.github.com>
2020-10-17 12:58:52 +0800

    Update main.lua
    Can now upload data.csv from main.lua folder.
    Can now get each element from each line and store it into an empty table for future use.
    -- Need to make a table (tried to use 320x 600 but that was too big for phone size, might need to change resolution of app to fix problem

--- Assignmnet temp/main.lua
+++ Assignmnet temp/main.lua
@@ -10,7 +10,13 @@ local widget = require( "widget" ) -- require widget
 -- hide status bar
 display.setStatusBar( display.HiddenStatusBar)
 
--- set exit about to invisible
+-- Create empty table
+local CapturedLine = {}
+------------------------------------------------------------------------------------------- Make Group
+-- make groups
+local helpGroup = display.newGroup()
+local graphGroup = display.newGroup()
+
 ------------------------------------------------------------------------------------------- The Algorithms
 local algorithmList = {"Algorithm 1", "Algorithm 2", "Algorithm 3"}
 choice = 0
@@ -18,28 +24,43 @@ choice = 0
 ------------------------------------------------------------------------------------------- create Algorithm name
 local scrollRect = display.newRect( 0, 0, display.contentWidth-30, 40)
 scrollRect:setFillColor( 1, 1, 1 )
-scrollRect.x = display.contentCenterX; scrollRect.y = display.contentHeight-74
-algorithmDisplay = display.newText("Select Algorithm",display.contentCenterX ,display.contentHeight-74 ,"Arial", 20)
+scrollRect.x = display.contentCenterX; scrollRect.y = display.screenOriginY +  450
+algorithmDisplay = display.newText("Select Algorithm",display.contentCenterX ,display.screenOriginY +  450 ,"Arial", 14)
 algorithmDisplay: setFillColor(0,0,0,1)
-
 ------------------------------------------------------------------------------------------- help text box info
 -- info here will go into help text box
-HelpTextInfo = ("This is some text\nThis is some more text\nWhat the hell is this fucking assignment ")
+local HelpTextInfo = ("This is some text\nThis is some more text\nWhat the hell is this fucking assignment ")
 
--- create 
-textBox = display.newRect(display.contentCenterX, display.contentCenterY-40,display.contentWidth-18,display.contentHeight-120); textBox.alpha = 0
-DisplayHelpText = display.newText(HelpTextInfo, display.contentCenterX - 10, display.contentCenterY-190, native.systemFont, 15); DisplayHelpText.alpha = 0
+-- create text box
+local helptextBox = display.newRect(display.contentCenterX, display.contentCenterY-40,display.contentWidth-18,display.contentHeight-120);-- helptextBox.alpha = 0
+local DisplayHelpText = display.newText(HelpTextInfo, display.contentCenterX - 10, display.contentCenterY-190, native.systemFont, 15);-- DisplayHelpText.alpha = 0
 DisplayHelpText:setFillColor(1,0,0)
+
+--peroblem with making the graph
+--[[
+-- create graph
+local graphBox = display.newRect(display.contentCenterX, display.contentCenterY-40,320,620); -- graphBox.alpha = 1
+local graphYLine = display.newLine(display.contentCenterX-150, display.contentCenterY-190,display.contentCenterX-150, display.contentCenterY+110); -- graphYLine.alpha = 1
+local graphXLine = display.newLine(display.contentCenterX+150, display.contentCenterY+110,display.contentCenterX-150, display.contentCenterY+110); -- graphXLine.alpha = 1
+graphYLine:setStrokeColor( 1, 0, 0, 1 ); graphYLine.strokeWidth = 2
+graphXLine:setStrokeColor( 1, 0, 0, 1 ); graphXLine.strokeWidth = 2
+]]
+
+helpGroup:insert(helptextBox); helpGroup:insert(DisplayHelpText); 
+helpGroup.alpha = 0
+--graphGroup:insert(graphBox);graphGroup:insert(graphYLine);graphGroup:insert(graphXLine);
+--graphGroup.alpha = 1
 ------------------------------------------------------------------------------------------- create Algorithm functions
 --------------- Change Algorithm left
 --Function to handle AlgorithmLeft button events, this will want to move to the previous algorithm option in a list
-local function AlgorithmLeftButtonEvent(event)
+function AlgorithmLeftButtonEvent(event)
  
     if ("ended" == event.phase) then
         choice = choice - 1
         if (choice <= 0) then
             choice = #algorithmList
-        end
+        end        
+        --print(choice)
         print(algorithmList[choice].." selected")
         algorithmDisplay.text = algorithmList[choice]
     end
@@ -47,13 +68,14 @@ end
 
 --------------- Change Algorithm right
 --Function to handle AlgorithmRight button events, this will want to move to the next algorithm option in a list
-local function AlgorithmRightButtonEvent(event)
+function AlgorithmRightButtonEvent(event)
  
     if ("ended" == event.phase) then
         choice = choice + 1
         if (choice > #algorithmList) then
             choice = 1
-        end
+        end        
+        print(choice)
         print(algorithmList[choice].." selected")
         algorithmDisplay.text = algorithmList[choice]
         --apply the actual algorithm
@@ -62,49 +84,61 @@ end
 
 --------------- About program
 --Function to handle AboutApp button events, this will need two different phases, one two open the about, and one to close it again
-local function AboutAppButtonEvent(event)
+function AboutAppButtonEvent(event)
     if ( event.numTaps == 1 ) then
-        textBox.alpha = 1
-        DisplayHelpText.alpha = 1
+        graphGroup.alpha = 0; helpGroup.alpha = 1
     elseif ( event.numTaps == 2 ) then
-        textBox.alpha = 0
-        DisplayHelpText.alpha = 0
+        helpGroup.alpha = 0
     end  
 end
 
 --------------- Upload data
 --Function to handle UploadFile button events, this will want to upload a file into the project resources, and read in the data
-local function UploadFileButtonEvent(event)
+function UploadFileButtonEvent(event)
     
     if ("ended" == event.phase) then
-        local path = system.pathForFile( "data.csv", system.DocumentsDirectory )
-        
+        local path = system.pathForFile("data.csv", system.ResorceDirectory)
+        -- Create empty table
+        local CapturedLine = {}
         -- Open the file handle
-        local file, errorString = io.open( path, "r" )
-        
+        local file, errorString = io.open( path, "r")
+
         if not file then
-            -- Error occurred; output the cause
-            print( "File error: " .. errorString )
+            -- Error occured; output cause
+            print("Error: " .. errorString)
         else
-            -- Output lines
+            -- Display Graph
+            --graphGroup.alpha = 1
+            -- Else capture every element seperately in each line
+            local counter = 1
             for line in file:lines() do
-                print( line )
-                --confirm data has been uploaded
-                algorithmDisplay.text = "Data Uploaded"
+                local yAxisChar, xAxisChar, groupTypeChar = line:match("([^Y]*),([^X]*),([^G]*)")
+                CapturedLine[counter] = {yAxisNum = tonumber(yAxisChar), xAxisNum = tonumber(xAxisChar), groupType = groupTypeChar}
+                counter = counter+1
             end
-            
             -- Close the file handle
             io.close( file )
         end
-        
-        file = nil
 
+        file = nil
+        -- Make total variable max of CapturedLine table
+        local total = table.maxn(CapturedLine)
+        local counter = 1
+        -- Make loop that prints each element in all lines of CapturedLine table
+        for num = 1, total, 1 do
+            print(CapturedLine[counter].yAxisNum ..', '..CapturedLine[counter].xAxisNum..','..CapturedLine[counter].groupType)
+            counter = counter + 1
         end
     end
+end
 
----------------- Apply alogrithm on data
+
+
+
+
+--------------- Apply alogrithm on data
 --Function to handle Submit button events,  this will want to apply the desired algorithm to the supplies data, and display the results on screen
-local function ApplyButtonEvent(event)
+function ApplyButtonEvent(event)
  
     if ("ended" == event.phase) then
         if (choice == 1) then
@@ -121,7 +155,7 @@ local function ApplyButtonEvent(event)
 end
 
 --------------- Exit program
-local function QuitButtonEvent(event)
+function QuitButtonEvent(event)
  
     if ("ended" == event.phase) then
         native.requestExit()
@@ -167,7 +201,6 @@ local buttonRight = widget.newButton(
 -- Create the about button widget
 local buttonAbout = widget.newButton(
     {
-
         id = "babout",
         label = "?",
         onEvent = AboutAppButtonEvent,
@@ -180,6 +213,7 @@ local buttonAbout = widget.newButton(
         strokeWidth = 1
     }
 )
+
 -- Create the upload button widget
 local buttonUpload = widget.newButton(
     {
@@ -253,5 +287,3 @@ buttonApply.x = display.screenOriginX + 276; buttonApply.y = display.screenOrigi
 -- Exit button placement
 --buttonExit.x =  display.contentCenterX +125; buttonExit.y = display.contentHeight -460
 buttonExit.x = display.screenOriginX + 285; buttonExit.y = display.screenOriginY + 40
-
-
